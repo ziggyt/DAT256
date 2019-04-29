491e7773a0bd5700c43fa55abbd52aaca71d4717
Albin <Albin644@gmail.com>
2019-04-29 10:29:30 +0200

    Create detailview for trips

---
+++ app/src/main/java/com/muk/sami/TripDetailViewFragment.java
@@ -0,0 +1,61 @@
+package com.muk.sami;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class TripDetailViewFragment extends Fragment {
+
+    private TextView textViewFrom;
+    private TextView textViewTo;
+    private TextView textViewDate;
+    private TextView textViewSeats;
+    private TextView textViewTime;
+    //private Button button;
+
+    private FirebaseFirestore mDatabase;
+    //private CollectionReference mTripsRef;
+
+    private View view;
+
+    public TripDetailViewFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_trip_detailview, container, false);
+
+        mDatabase = FirebaseFirestore.getInstance();
+
+        textViewFrom = view.findViewById(R.id.textview_from);
+        textViewTo = view.findViewById(R.id.textview_to);
+        //textViewDate = view.findViewById(R.id.textview_date);
+        //textViewSeats = view.findViewById(R.id.textview_seats);
+        //textViewTime = view.findViewById(R.id.textview_time);
+
+
+
+        return view;
+    }
+
+}
