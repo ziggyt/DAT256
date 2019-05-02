d74d5400381af6c3775e5e0963736f209959270d
Albin <Albin644@gmail.com>
2019-05-02 18:28:09 +0200

    Add textView to listitem_trip showing number of free seats

--- app/src/main/java/com/muk/sami/TripListAdapter.java
+++ app/src/main/java/com/muk/sami/TripListAdapter.java
@@ -1,7 +1,6 @@
 package com.muk.sami;
 
 import android.app.Activity;
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +9,6 @@ import android.widget.TextView;
 
 import com.muk.sami.model.Trip;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class TripListAdapter extends ArrayAdapter <Trip> {
@@ -28,18 +26,23 @@ public class TripListAdapter extends ArrayAdapter <Trip> {
     public View getView(int position, View convertView, ViewGroup parent) {
 
         LayoutInflater inflater = context.getLayoutInflater();
-        View listitem = inflater.inflate(R.layout.layout_list, null, true);
+        View listitem = inflater.inflate(R.layout.listitem_trip, null, true);
 
-        TextView textViewFrom = (TextView) listitem.findViewById(R.id.textView_From);
-        TextView textViewTo = (TextView) listitem.findViewById(R.id.textView_To);
-        TextView textViewDate = (TextView) listitem.findViewById(R.id.textView_Date);
-        TextView textViewTime = (TextView) listitem.findViewById(R.id.textView_Time);
+        TextView textViewFrom  = listitem.findViewById(R.id.textView_From);
+        TextView textViewTo    = listitem.findViewById(R.id.textView_To);
+        TextView textViewDate  = listitem.findViewById(R.id.textView_Date);
+        TextView textViewTime  = listitem.findViewById(R.id.textView_Time);
+        TextView textViewSeats = listitem.findViewById(R.id.textView_Seats);
 
         Trip trip = trips.get(position);
+
         textViewFrom.setText(trip.getFrom());
         textViewTo.setText(trip.getTo());
         textViewDate.setText(trip.getDate());
         textViewTime.setText(trip.getTime());
+
+        int remainingSeats = trip.getTotalNumberOfSeats() - trip.getNumberOfBookedSeats();
+        textViewSeats.setText(remainingSeats + " platser kvar");
 
         return listitem;
     }
