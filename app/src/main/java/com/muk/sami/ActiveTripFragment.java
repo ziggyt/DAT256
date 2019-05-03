6c33e7e041999061cd491e316c4680a16ad16306
ziggyt <33518806+ziggyt@users.noreply.github.com>
2019-05-03 17:48:15 +0200

    Add new ActiveTrip fragment

---
+++ app/src/main/java/com/muk/sami/ActiveTripFragment.java
@@ -0,0 +1,55 @@
+package com.muk.sami;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+
+public class ActiveTripFragment extends Fragment {
+
+    private View view;
+    private ImageView qrImageView;
+    private String tripId;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.fragment_active_trip, container, false);
+
+        qrImageView = view.findViewById(R.id.qr_image_view);
+
+        tripId = ActiveTripFragmentArgs.fromBundle(getArguments()).getTripId();
+
+        try {
+            Bitmap qrCode = createQrBitmap();
+            qrImageView.setImageBitmap(qrCode);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+
+        return view;
+    }
+
+    private Bitmap createQrBitmap() throws WriterException {
+        String textToEncode = tripId;
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+
+        BitMatrix bitMatrix = multiFormatWriter.encode(textToEncode, BarcodeFormat.QR_CODE, 500, 500);
+        BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+        return barcodeEncoder.createBitmap(bitMatrix);
+    }
+
+}
