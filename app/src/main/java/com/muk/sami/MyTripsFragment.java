447abeaaf49eb42eaee021dcd7610e63e1607f58
Albin <Albin644@gmail.com>
2019-05-06 18:44:30 +0200

    Add new Fragment "MyTripsFragment" and connect it to the navigation bar

---
+++ app/src/main/java/com/muk/sami/MyTripsFragment.java
@@ -0,0 +1,35 @@
+package com.muk.sami;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MyTripsFragment extends Fragment {
+
+    View view;
+
+
+    public MyTripsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_my_trips, container, false);
+
+
+
+        return view;
+    }
+
+
+}
