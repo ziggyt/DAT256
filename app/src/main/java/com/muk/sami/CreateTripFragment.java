f8654249c2f9ec089fac07f889e7c8b4284548d0
petrusoskarsson <pde.oskarsson@gmail.com>
2019-05-09 12:58:20 +0200

    add button for adding a trip

--- app/src/main/java/com/muk/sami/CreateTripFragment.java
+++ app/src/main/java/com/muk/sami/CreateTripFragment.java
@@ -9,10 +9,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.muk.sami.model.Trip;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class CreateTripFragment extends Fragment {
     private EditText fromEditText;
@@ -21,6 +31,10 @@ public class CreateTripFragment extends Fragment {
     private TextView dateTextView;
     private DatePicker datePicker;
     private TimePicker timePicker;
+    private Button addTripButton;
+
+    private FirebaseFirestore mDatabase;
+    private CollectionReference mTripsRef;
 
     private View view;
 
@@ -36,8 +50,48 @@ public class CreateTripFragment extends Fragment {
         dateTextView = view.findViewById(R.id.textViewDate);
         datePicker = view.findViewById(R.id.datePicker);
         timePicker = view.findViewById(R.id.timePicker);
+        addTripButton = view.findViewById(R.id.addTripButton);
+
+        mDatabase = FirebaseFirestore.getInstance();
+        mTripsRef = mDatabase.collection("trips");
+
+        addTripButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String from = fromEditText.getText().toString();
+                String to = toEditText.getText().toString();
+                String seats = seatsEditText.getText().toString();
+                Date date = dateFromDatePicker(datePicker, timePicker);
+
+                createTrip(from, to, date, Integer.parseInt(seats));
+            }
+        });
 
         return inflater.inflate(R.layout.fragment_create_trip, container, false);
+    }
+
+    private Date dateFromDatePicker(DatePicker p, TimePicker t){
+        int year = p.getYear();
+        int month = p.getMonth();
+        int day = p.getDayOfMonth();
+        int hour = t.getHour();
+        int min = t.getMinute();
+
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.set(year, month, day, hour, min);
+
+        return calendar.getTime();
+    }
+
+    private void createTrip(String from, String to, Date date, int seats) {
+
+        String tripId = mTripsRef.document().getId();
+        String driverId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        Trip trip = new Trip(tripId, from, to, date,0 , seats, driverId);
+        mTripsRef.document(tripId).set(trip);
+
+        Toast.makeText(getContext(), "Resa tillagd", Toast.LENGTH_LONG).show(); //TODO replace with string value
     }
 
 }
