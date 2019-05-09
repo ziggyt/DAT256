6d272e0bc029dca6687fb757e9d4ddb20ea272b7
nojjeN <niklas.adam.andersson@gmail.com>
2019-05-09 23:19:42 +0200

    Create new fragment with xml for a driver detailView

---
+++ app/src/main/java/com/muk/sami/DriverDetailViewFragment.java
@@ -0,0 +1,24 @@
+package com.muk.sami;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class DriverDetailViewFragment extends Fragment {
+
+    View view;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_driver_detail_view, container, false);
+
+        return view;
+    }
+}
