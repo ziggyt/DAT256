a0aff5803f4f02898a379fc954db3cf0177a5785
nojjeN <niklas.adam.andersson@gmail.com>
2019-05-14 21:19:46 +0200

    Add on the adapter on the listView

--- app/src/main/java/com/muk/sami/PassengerListAdapter.java
+++ app/src/main/java/com/muk/sami/PassengerListAdapter.java
@@ -12,12 +12,12 @@ import com.muk.sami.model.User;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PassengerListAdapter extends ArrayAdapter<User> {
+public class PassengerListAdapter extends ArrayAdapter<String> {
     private Activity mContext;
-    private List<User> passengers;
+    private List<String> passengers;
 
 
-    public PassengerListAdapter(Activity context, ArrayList<User> objects) {
+    public PassengerListAdapter(Activity context, List<String> objects) {
         super(context, 0, objects);
         this.mContext = context;
         this.passengers = objects;
@@ -28,9 +28,9 @@ public class PassengerListAdapter extends ArrayAdapter<User> {
         View view = inflater.inflate(R.layout.listitem_trip, null, true);
         TextView passengerNameTextView = view.findViewById(R.id.passenger_name_text_view);
 
-        User passenger = passengers.get(position);
+        String passenger = passengers.get(position);
 
-        passengerNameTextView.setText(passenger.getDisplayName());
+        passengerNameTextView.setText(passenger);
 
         return view;
     }
