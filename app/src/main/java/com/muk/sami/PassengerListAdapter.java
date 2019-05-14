14638c038e80bdef5bf8fdeb5545d43eac6dda0e
Albin <Albin644@gmail.com>
2019-05-15 00:07:29 +0200

    Add a list in Trip class which holds the statuses of the passengers. When a passengers finishes a trip, their status changes, and the driver will be able to see the result in the detailView

--- app/src/main/java/com/muk/sami/PassengerListAdapter.java
+++ app/src/main/java/com/muk/sami/PassengerListAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import com.muk.sami.model.User;
@@ -15,22 +16,31 @@ import java.util.List;
 public class PassengerListAdapter extends ArrayAdapter<String> {
     private Activity mContext;
     private List<String> passengers;
+    private List<String> passengersStatus;
 
 
-    public PassengerListAdapter(Activity context, List<String> objects) {
-        super(context, 0, objects);
+    public PassengerListAdapter(Activity context, List<String> passengers, List<String> passengersStatus) {
+        super(context, 0, passengers);
         this.mContext = context;
-        this.passengers = objects;
+        this.passengers = passengers;
+        this.passengersStatus = passengersStatus;
     }
 
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = mContext.getLayoutInflater();
         View view = inflater.inflate(R.layout.passenger_list_item, null, true);
+
         TextView passengerNameTextView = view.findViewById(R.id.passenger_name_text_view);
+        CheckBox checkBox = view.findViewById(R.id.checkBox);
 
         String passenger = passengers.get(position);
+        String passengerStatus = passengersStatus.get(position);
 
         passengerNameTextView.setText(passenger);
+
+        if (passengerStatus.equals("Finished trip")){
+            checkBox.setChecked(true);
+        }
 
         return view;
     }
