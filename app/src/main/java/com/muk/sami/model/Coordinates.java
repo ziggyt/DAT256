aa5ad387cd7d612d2f2122410f28ff0a422a083d
nojjeN <niklas.adam.andersson@gmail.com>
2019-05-22 13:26:45 +0200

    Small change to Coordinate class

--- app/src/main/java/com/muk/sami/model/Coordinates.java
+++ app/src/main/java/com/muk/sami/model/Coordinates.java
@@ -1,10 +1,5 @@
 package com.muk.sami.model;
-
-import android.os.Parcel;
-import android.os.Parcelable;
-
 import java.io.Serializable;
-import java.util.Comparator;
 
 import lombok.Getter;
 import lombok.ToString;
@@ -29,10 +24,10 @@ public class Coordinates implements Serializable {
 
         double lat1 = lat;
         double lon1 = lon;
-        double lat2 = otherLocation.getLat();
-        double lon2 = otherLocation.getLon();
+        double lat2 = otherLocation.lat;
+        double lon2 = otherLocation.lon;
 
-        final int R = 6371; // Radius of the earth
+        final int radiusOfEarth = 6371; // Radius of the earth
 
         double latDistance = Math.toRadians(lat2 - lat1);
         double lonDistance = Math.toRadians(lon2 - lon1);
@@ -40,11 +35,9 @@ public class Coordinates implements Serializable {
                 + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
                 * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
         double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        double distance = R * c * 1000;
+        double distance = radiusOfEarth * c * 1000;
 
-        distance = Math.pow(distance, 2);
-
-        return Math.sqrt(distance);
+        return Math.abs(distance);
 
     }
 }
